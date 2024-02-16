5cd172df2cc322caa57c7e3a7750bff76ddba1e9
vinas.296768 <vinas.296768@LAB-B-PC07>
2024-02-16 09:35:47 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DataType
+{
+    class DataTypeProgram
+    {
+        static void Main()
+        {
+            Console.Write("Enter the pieces of apple: ");
+            int piecesOfApple = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter total price of " + piecesOfApple + " apple(s): ");
+            double priceOfApple = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine"The total price of " + piecesOfApple + "pieces(s) is " + priceOfApple);
+
+            Console.WriteLine("The value of original price is " + priceOfApple);
+
+            Console.WriteLine("The value of converted price is " + (int)priceOfApple);
+
+            Console.WriteLine("Press any key to exit.... ");
+            Console.ReadKey();
+        }
+    }
+}
+
+ 
